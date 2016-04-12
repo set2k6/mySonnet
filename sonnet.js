8600199f9f8385c97b28950cfa5050aac220a484
set2k6 <set2k6@gmail.com>
2016-04-11 23:23:13 -0500

    First commit

--- sonnet.js
+++ sonnet.js
@@ -42,3 +42,46 @@ for (var i = Things.length - 1; i >= 0; i--) {
 for (var i = 0; i < Things.length; i++) {
 	Things[i]
 }
+
+// ############## for loops #####################
+// Write a for loop that increments the counter variable by 10 each time, and displays the value.
+
+// Write a for loop that divides the counter variable by 2 each time, and displays the value.
+
+// Augment the loop to insert each new value into an array
+
+// Write a loop that starts at 100 and decrements a variable by 1. If the number is even, add the number
+//to the beginning of an array, else add it to the end of the array.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
